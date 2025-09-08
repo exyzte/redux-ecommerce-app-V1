448577107b5d3cf88adb41196315e8ca23e871e9
exyzte <davidecicerys@gmail.com>
2025-09-08 00:03:52 -0400

    completed shopping cart functionality to handle state reducer and successfully render component

--- src/Components/ShoppingCart.jsx
+++ src/Components/ShoppingCart.jsx
@@ -1,18 +1,46 @@
 import React from 'react';
-import './ShoppingCart.css'; 
+import { useDispatch, useSelector } from 'react-redux';
+import { removeItemFromCart, clearCart, increaseItemQuantity, decreaseItemQuantity } from './CartSlice';
+import './ShoppingCart.css';
+import ProductList from './ProductList';
 
 const ShoppingCart = () => {
+const dispatch = useDispatch();
+const cartItems = useSelector(state => state.cart.cartItems);
+const totalAmount = cartItems.reduce((total, item) => total + item.price * item.quantity, 0);
+
+const handleRemoveItem = itemId => {
+    dispatch(removeItemFromCart(itemId));
+};
+const handleClearCart = () => {
+    dispatch(clearCart());
+};
+const handleIncreaseQuantity = itemId => {
+    dispatch(increaseItemQuantity(itemId));
+};
+const handleDecreaseItemQuantity = itemId => {
+    dispatch(decreaseItemQuantity(itemId));
+};
 
   return (
     <>
     <div className="shopping-cart">
       <h2 className="shopping-cart-title">Shopping Cart</h2>
       <ul className="cart-items">
-       
+        {cartItems.map(item => (
+            <li key={item.id} className="cart-item" >
+                <span>{item.name} - ${item.price}</span>
+                <div className="quantity-controls">
+                    <button className="quantity-control-btn" onClick={() => handleIncreaseQuantity(item.id)}>+</button>
+                    <button hidden={item.quantity < 2} className="quantity-control-btn" onClick={() => handleDecreaseItemQuantity(item.id)}>-</button>
+                </div>
+                    <button className="remove-item-btn" onClick={() => handleRemoveItem(item.id)}>Remove Item</button>
+            </li>
+        ))}
       </ul>
-      <button className="clear-cart-btn">Clear Cart</button>
+      <button className="clear-cart-btn" onClick={() => handleClearCart()}>Clear Cart</button>
+      <div>{totalAmount ? <div>'The total amount is {totalAmount}'</div> : ''}</div>
     </div>
-  
     </>
   );
 };
